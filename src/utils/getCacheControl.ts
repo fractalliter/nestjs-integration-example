b5cb09a0ff9b6ae88d2f58ec487841cc4f865816
Elias Rahmani <eliass.rahmani@gmail.com>
2021-12-12 14:47:31 +0330

    add optional mark to headers for cache-contorl

--- src/utils/getCacheControl.ts
+++ src/utils/getCacheControl.ts
@@ -1,7 +1,7 @@
 import {AxiosResponseHeaders} from "axios";
 
 export default (headers: AxiosResponseHeaders): number => {
-	const matches = headers['cache-control'].match(/max-age=(\d+)/)
+	const matches = headers['cache-control']?.match(/max-age=(\d+)/)
 	const maxAge = matches ? parseInt(matches[1], 10) : -1
 	return maxAge;
 }
